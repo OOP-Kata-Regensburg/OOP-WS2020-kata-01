6e30c49cc6e6af0eda13994e67a46c08ba62ad20
javapyt <daniel.kwoska@stud.uni-regensburg.de>
2020-12-15 13:44:25 +0100

    changed failure description to german, changed Kata description, every exercise is now called a Kata

--- src/Kata.java
+++ src/Kata.java
@@ -3,28 +3,28 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-   KataTest.class
+        KataTest.class
 })
 
 public class Kata {
-    
-    /* 1) Schreiben Sie eine Methode, die ein int[] als Parameter übergeben bekommt und die größte Zahl in diesem Array zurückgibt. */
+
+    /* Kata 1 Schreiben Sie eine Methode, die ein int[] als Parameter übergeben bekommt und die größte Zahl in diesem Array zurückgibt. */
     public static int kataIntArrayMaximum(int[] values) {
-      return 0;
+        return 0;
     }
 
-    /* 2) Schreiben Sie eine Methode, die ein int[] als Parameter übergeben bekommt, die den Durchschnittswert aller gespeicherten Integers des Arrays berechnet und zurückgibt.*/
+    /* Kata 2 Schreiben Sie eine Methode, die ein int[] als Parameter übergeben bekommt, die den Durchschnittswert aller gespeicherten Integers des Arrays berechnet und zurückgibt.*/
     public static int kataIntArrayAverage(int[] values) {
-      return 0;
-    }
-    
-    /* 3) Schreiben Sie eine Methode, die einen String als Parameter übergeben bekommt und in umgekehrter Reihenfolge zurückgibt.*/
-    public static String kataStringReversed(String str) {
-      return "";
+        return 0;
     }
 
-    /* 4) Schreiben Sie eine Methode, die einen String als Parameter übergeben bekommt und jedes auftretende 'a' durch ein 'e' ersetzt. */
+    /* Kata 3 Schreiben Sie eine Methode, die einen String als Parameter übergeben bekommt und in umgekehrter Reihenfolge zurückgibt.*/
+    public static String kataStringReversed(String str) {
+        return "";
+    }
+
+    /* Kata 4 Schreiben Sie eine Methode, die einen String als Parameter übergeben bekommt und jedes auftretende 'a' durch ein 'e' ersetzt. */
     public static String kataStringReplaceA(String str) {
-      return "";
+        return "";
     }
 }
--- src/Tests/KataTest.java
+++ src/Tests/KataTest.java
@@ -8,96 +8,96 @@ import junit.framework.JUnit4TestAdapter;
 import org.junit.AfterClass;
 public class KataTest {
 
-private int[] values; 
-private String testString; 
-public static final String ANSI_RED = "\u001B[31m";
-public static final String ANSI_GREEN = "\u001B[32m";
-private static int correctMethods = 0; 
+    private int[] values;
+    private String testString;
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    private static int correctMethods = 0;
     // declare
-  
+
     @Before
-    public void setUp() throws Exception 
+    public void setUp() throws Exception
     {
-      // define
-      values = new int[] {1, 2, 3, 4, 5, 6, 7};
-      testString = "regal";
+        // define
+        values = new int[] {1, 2, 3, 4, 5, 6, 7};
+        testString = "regal";
     }
 
     @Test
     public void kataIntArrayMaximum() {
-      String str = "Failure: The maximum value is expected to be %d which is not equal to the result of kataIntArrayMaximum which returned %d!";
+        String str = "Fehler bei Aufgabe 1: Der höchste Wert ist %d, der Algorithmus in kataIntArrayMaximum hat stattdessen %d zurückgegeben!";
 
-      int testValue = Kata.kataIntArrayMaximum(values);
+        int testValue = Kata.kataIntArrayMaximum(values);
 
-      try {
-        assertEquals(String.format(str, 7, testValue), 7, testValue);
-        correctMethods++;   
+        try {
+            assertEquals(String.format(str, 7, testValue), 7, testValue);
+            correctMethods++;
 
-      } catch (AssertionError ae) {
-        System.out.println("\n" + ANSI_RED + String.format(str, 7, testValue));
-      }
+        } catch (AssertionError ae) {
+            System.out.println("\n" + ANSI_RED + String.format(str, 7, testValue));
+        }
     }
 
     @Test
     public void kataIntArrayAverage() {
-      String str = "Failure: The average value is expected to be %d which is not equal to the result kataIntArrayAverage which returned %d!";
+        String str = "Fehler bei Aufgabe 2: Der Durchschnittswert ist %d, der Algorithmus in kataIntArrayAverage hat stattdessen %d zurückgegeben!";
 
-      int testValue = Kata.kataIntArrayAverage(values);
+        int testValue = Kata.kataIntArrayAverage(values);
 
 
-       try{
-        assertEquals(String.format(str, 4, testValue), 4, testValue); 
-        correctMethods++;  
+        try{
+            assertEquals(String.format(str, 4, testValue), 4, testValue);
+            correctMethods++;
 
-       } catch (AssertionError ae){
-        System.out.println("\n" + ANSI_RED + String.format(str, 4, testValue));
-       }
+        } catch (AssertionError ae){
+            System.out.println("\n" + ANSI_RED + String.format(str, 4, testValue));
+        }
     }
 
     @Test
-    public void kataStringReversed() 
+    public void kataStringReversed()
     {
-      String str = "Failure: The average value is expected to be %s which is not equal to the result kataStringReversed which returned %s!";
+        String str = "Fehler bei Aufgabe 2: Der String lautet umgekehrt: %s der Algorithmus in kataStringReversed hat stattdessen %s zurückgegeben!";
 
-      String testValue = Kata.kataStringReversed(testString);
+        String testValue = Kata.kataStringReversed(testString);
 
-      try{
-        assertEquals("lager", Kata.kataStringReversed(testString));
-        correctMethods++;  
+        try{
+            assertEquals("lager", Kata.kataStringReversed(testString));
+            correctMethods++;
 
-      } catch (AssertionError ae){
-          System.out.println("\n" + ANSI_RED + String.format(str, "lager", testValue));
-      }
+        } catch (AssertionError ae){
+            System.out.println("\n" + ANSI_RED + String.format(str, "lager", testValue));
+        }
     }
-    
+
     @Test
-    public void kataStringReplaceA() 
+    public void kataStringReplaceA()
     {
-      String str = "Failure: The average value is expected to be %s which is not equal to the result kataStringReplaceA which returned %s!";
-      
-      String testValue = Kata.kataStringReplaceA(testString);
+        String str = "Fehler bei Aufgabe 4: Der String nach ersetzen aller 'a' mit 'e' Vokalen lautet : %s der Algorithmus in kataStringReplacedA hat stattdessen %s zurückgegeben!";
 
-      try{
-        assertEquals("regel", Kata.kataStringReplaceA(testString));
-        correctMethods++;  
+        String testValue = Kata.kataStringReplaceA(testString);
 
-      } catch (AssertionError ae){
-          System.out.println("\n" + ANSI_RED + String.format(str, "regel", testValue));
-      }
+        try{
+            assertEquals("regel", Kata.kataStringReplaceA(testString));
+            correctMethods++;
+
+        } catch (AssertionError ae){
+            System.out.println("\n" + ANSI_RED + String.format(str, "regel", testValue));
+        }
     }
 
     @AfterClass
     public static void checkIfAllCorrect(){
-      if (correctMethods == 4){
-        System.out.println(ANSI_GREEN + "all correct. Great!");
-      }
+        if (correctMethods == 4){
+            System.out.println(ANSI_GREEN + "all correct. Great!");
+        }
     }
     public static void main(String[] args) {
-      junit.textui.TestRunner.run(suite());
+        junit.textui.TestRunner.run(suite());
     }
 
-    public static junit.framework.Test suite() 
+    public static junit.framework.Test suite()
     {
-      return new JUnit4TestAdapter(KataTest.class);
+        return new JUnit4TestAdapter(KataTest.class);
     }
 }
